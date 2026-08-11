3b4801e82b92efe362bb0d32f8027c6f54645f05
u23urwurhuih <ilrsamar@brc.pshs.edu.ph>
2026-08-11 20:34:06 +0800

    .

--- zodiacSection_Beryllium.py
+++ zodiacSection_Beryllium.py
@@ -1,1 +1,6 @@
-
+a = int(input("Enter your birth year: "))
+if a < 1900:
+  print("Year of birth must not be earlier than the year 1900.")
+else:
+  if k:
+    print(f"Your Chinese Zodiac Sign is: {chinese_zodiac_sign}")
